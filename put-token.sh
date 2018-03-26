498c00259aa3d28fc305cc5d3757ccbd7f6db607
Mathias Pius <mathias@pius.io>
2018-03-26 12:05:50 +0200

    Added 10 second sleep to end of script, to allow dns-01 to reload zone files so LE doesn't query stale data and fails challenge

--- put-token.sh
+++ put-token.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "installing TXT record"
+echo "installing TXT record for \"$DNS01_APIKEY\""
 echo "$DNS01_RECORD IN TXT \"$CERTBOT_VALIDATION\""
-
 
 curl "http://api.dns-01.online/record" \
  -H "Content-Type: application/json" \
  -X POST \
  -d "{\"apikey\":\"$DNS01_APIKEY\",\"name\":\"$DNS01_RECORD\",\"token\":\"$CERTBOT_VALIDATION\",\"ttl\":86400}"
+
+
+# If we already have a TXT record on dns-01.online, LetsEncrypt might poll that
+# before we get to update it, and we'll fail the challenge for using a statle token.
+# By sleeping for a little while, we can be sure dns-01.online has had time to
+# publish our new token
+sleep 10
 
